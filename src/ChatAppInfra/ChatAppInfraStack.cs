f3968b9c170c0b23b5fc70197af2d6f546db6460
aloranking <alobently@outlook.com>
2026-02-24 11:20:06 +0100

    Initial commit

---
+++ src/ChatAppInfra/ChatAppInfraStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace ChatAppInfra
+{
+    public class ChatAppInfraStack : Stack
+    {
+        internal ChatAppInfraStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
